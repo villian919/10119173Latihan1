0ae46a60c170406e8695238fa641df6027fb3715
villian919 <villian919@gmail.com>
2022-04-16 15:44:21 +0700

    first commit

---
+++ app/src/main/java/com/example/a10119173latihan1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a10119173latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+////16 April 2022, 10119173, BIrliano Ekayan Arsyad, IF5
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
